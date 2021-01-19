dbabef5ae1e675100626ab363110ded6f1d3b55b
mona hm <m0na.mh12380@gmail.com>
2021-01-19 17:25:10 +0330

    third day commit

--- app/src/test/java/HomePageActivity.java
+++ app/src/test/java/HomePageActivity.java
@@ -4,8 +4,13 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.myapplication.R;
+import com.example.myapplication.ui.home.HomeFragment;
+import com.example.myapplication.ui.profile.ProfileFragment;
+import com.example.myapplication.ui.setting.SettingFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomePageActivity extends AppCompatActivity {
@@ -20,16 +25,25 @@ public class HomePageActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.nav_home:
-
+                        HomeFragment fragment1 = new HomeFragment();
+                        openFragment(fragment1);
                         break;
                     case R.id.nav_profile:
-
+                        ProfileFragment fragment2 = new ProfileFragment();
+                        openFragment(fragment2);
                         break;
                     case R.id.nav_setting:
-
+                        SettingFragment fragment3 = new SettingFragment();
+                        openFragment(fragment3);
                         break;
                 }
+                return true;
             }
         });
+    }
+    public void openFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frag_container, fragment);
+        transaction.commit();
     }
 }
